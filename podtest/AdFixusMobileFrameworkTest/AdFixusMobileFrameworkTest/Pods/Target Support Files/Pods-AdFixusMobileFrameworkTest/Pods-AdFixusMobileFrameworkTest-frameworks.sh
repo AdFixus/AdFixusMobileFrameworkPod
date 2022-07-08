fdcad21af2d8f562800d1090f6b8a35c6fdfd881
Marko Markovic <marko.markovic@adfixus.com>
2022-07-08 10:13:11 +1000

    No CocoaPods
    No CocoaPods

--- podtest/AdFixusMobileFrameworkTest/AdFixusMobileFrameworkTest/Pods/Target Support Files/Pods-AdFixusMobileFrameworkTest/Pods-AdFixusMobileFrameworkTest-frameworks.sh
+++ podtest/AdFixusMobileFrameworkTest/AdFixusMobileFrameworkTest/Pods/Target Support Files/Pods-AdFixusMobileFrameworkTest/Pods-AdFixusMobileFrameworkTest-frameworks.sh
@@ -179,13 +179,11 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PromisesObjC/FBLPromises.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
-  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/AdFixusMobileFramework/AdFixusMobileFramework.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PromisesObjC/FBLPromises.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
-  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/AdFixusMobileFramework/AdFixusMobileFramework.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
