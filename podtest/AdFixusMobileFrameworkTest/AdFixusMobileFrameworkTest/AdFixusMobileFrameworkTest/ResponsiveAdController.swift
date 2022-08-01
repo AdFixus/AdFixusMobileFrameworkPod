85a1fed9ab1f0c385c6229e06384727188a31d21
Marko Markovic <marko.markovic@adfixus.com>
2022-08-01 14:53:11 +1000

    Cleanup
    Cleanup

--- podtest/AdFixusMobileFrameworkTest/AdFixusMobileFrameworkTest/AdFixusMobileFrameworkTest/ResponsiveAdController.swift
+++ podtest/AdFixusMobileFrameworkTest/AdFixusMobileFrameworkTest/AdFixusMobileFrameworkTest/ResponsiveAdController.swift
@@ -9,13 +9,6 @@ import GoogleMobileAds
 import UIKit
 import AdFixusMobileFramework
 
-//https://guides.codepath.com/ios/Using-UICollectionView
-
-// https://stackoverflow.com/questions/46932341/class-is-implemented-in-both-one-of-the-two-will-be-used-which-one-is-undefine
-
-// READ THE ABOVE ARTICLE AND REMEMBER THAT -framework GoogleMobileAds was added twice
-// This issue has been resolved by automating .debug and .release Pods- from podfile (see code)
-
 class ResponsiveAdController: UIViewController, GADBannerViewDelegate, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UIGestureRecognizerDelegate  {
        
     @IBOutlet weak var collectionView: UICollectionView!
@@ -42,8 +35,6 @@ class ResponsiveAdController: UIViewController, GADBannerViewDelegate, UICollect
             if indexPath.row == 0 {
                 cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ListingCell", for: indexPath)
                 cell.backgroundColor = UIColor.white
-                
-                
                 
                 for subview in cell.subviews {
                     subview.removeFromSuperview()
@@ -75,8 +66,6 @@ class ResponsiveAdController: UIViewController, GADBannerViewDelegate, UICollect
                 cell.addSubview(bannerView)
                 bannerView.centerXAnchor.constraint(equalTo: cell.centerXAnchor).isActive = true
                 bannerView.centerYAnchor.constraint(equalTo: cell.centerYAnchor).isActive = true
-                 
-                
             }
             else {
                 cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ListingCell", for: indexPath)
@@ -85,8 +74,7 @@ class ResponsiveAdController: UIViewController, GADBannerViewDelegate, UICollect
         } else {
             cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AdCell", for: indexPath)
             cell.backgroundColor = UIColor.white
-            
-            
+
             for subview in cell.subviews {
                 subview.removeFromSuperview()
             }
@@ -106,7 +94,6 @@ class ResponsiveAdController: UIViewController, GADBannerViewDelegate, UICollect
             
             adSlotId += 1
             _ = loadAdWithParameters(adView: cell, targeting: &targeting)
-             
         }
 
         return cell
@@ -137,18 +124,6 @@ class ResponsiveAdController: UIViewController, GADBannerViewDelegate, UICollect
         
         collectionView.collectionViewLayout = layout
         collectionView.dragInteractionEnabled = false
-
-        /*
-        for recognizer in collectionView.gestureRecognizers ?? []
-        {
-            recognizer.isEnabled = false
-            if (recognizer is UILongPressGestureRecognizer)
-            {
-                recognizer.isEnabled = false
-            }
-            print("recognizer: \(String(describing: recognizer)) \")")
-        }
-         */
     }
     
     func loadAdWithParameters(adView: UIView, targeting: inout Dictionary<String, String>) -> OperationResponse
@@ -197,7 +172,6 @@ class ResponsiveAdController: UIViewController, GADBannerViewDelegate, UICollect
         let operationResponse = manager.loadResponsiveAd(self, adContainerUIView: adView, initialSize: size, adSizes: adSizes, adUnitID: adUnitID, customTargeting: &targeting, publisherProvidedID: nil, delegate: nil)
         
         return operationResponse
-        
     }
      
      
